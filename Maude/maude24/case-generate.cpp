fd537c46bd4f824d8bd7d0f108aeaf02de935130
jiaxiang <jiaxiang0924@gmail.com>
2016-03-27 02:01:14 +0800

    modified the testing codes

--- Maude/maude24/case-generate.cpp
+++ Maude/maude24/case-generate.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
+#include <math.h>
 
 using namespace std;
 
@@ -15,12 +17,31 @@ unsigned long long lcm(unsigned long long a, unsigned long long b) {
 }
 
 int main() {
+  /*
     int N = 5;
-    int periods[N];
+    int TESTNUM = 50;
+    int caseNum = 0;
+  */
+
+  //  int N = 10;
+  int N;
+  int TESTNUM = 10;
+  int caseNum = 0;
+
+  for (N = 5; N <= 20; N++) { 
+    cout << "Task number is: " << N << endl;
+    
     srand((unsigned)time(NULL));
+
+    for (caseNum = 0; caseNum < TESTNUM; caseNum++ ) {
+
+      
+    int periods[N];
     for(int i = 0; i < N; i++) {
-        periods[i] = 1 + (rand() % 200);
+      //        periods[i] = 1 + (rand() % 100);
+      periods[i] = pow(2, i);
     }
+    
     
     int i,j,temp;
     for (i = 0 ; i < N; i++) {
@@ -34,14 +55,87 @@ int main() {
     }
     
     
-    for(int i = 0; i < N; i++) {
-        cout << periods[i] << endl;
-    }    
-
     unsigned long long macro = 1;
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < N; i++) {
         macro = lcm(macro, periods[i]);
     }
+    cout << "round: " << caseNum << endl;
     cout << macro << endl;
+
+    int computations[N];
+    for(int i = 0; i < N; i++) {
+        computations[i] = 1 + (rand() % (5000 * periods[i] / N));
+    }
+    
+    for(int i = 0; i < N; i++) {
+      cout << "period: " << periods[i] * 5 << ", computation: " << computations[i] << endl;
+    }
+
+
+    // write to the file
+
+    //    ofstream out(to_string(N) + "/test-case-" + to_string(caseNum) + ".maude");
+    ofstream out("pow/" + to_string(N) + "-" + to_string(caseNum) + ".maude");
+    if (out.is_open()) {
+      out << "set trace off ." << endl;
+      out << "set break off ." << endl;
+      out << "set profile off ." << endl;
+      out << endl;
+
+      out << "(tomod INSTANTIATION is " << endl;
+      out << "  protecting POSRAT-TIME-DOMAIN-WITH-INF ." << endl;
+      out << "  including INTERRUPT-SOURCE ." << endl;
+      out << "  including PERIODIC-TASK ." << endl;
+      out << "  including SYSTEM ." << endl;
+      out << "  including RMS-VERIFICATION ." << endl;
+      out << "  including RMS-MODEL-CHECK ." << endl;
+      out << "  protecting QID ." << endl;
+      out << "  subsort Qid < Oid ." << endl;
+      out << endl;
+      
+      out << "  op tisrc : -> Object ." << endl;
+      out << "  eq tisrc = < 'tisrc : IntSrc | val : 0 , cycle : 5000 > ." << endl;
+      out << endl;
+
+      out << "  op tsts : -> SysTasks ." << endl;
+      out << "  eq tsts = [ < scheduling : Task | cnt : [ 0 / [ 38 , 38 ] ] > < switching : Task | cnt : [ 0 / [ 20 , 20 ] ] > ] ." << endl;
+      out << endl;
+ 
+      out << "  op tHW : -> Hardware ." << endl;
+      out << "  eq tHW = [ < 'tr : Regs | pc : none , mask : false , ir : false , temp : none > ; bottom ] ." << endl;
+      out << endl;
+
+      out << "  op tl : -> TaskList ." << endl;
+      string tasks[N];
+      for (i = 0; i < N; i++) {
+	tasks[i] = "< 't" + to_string(i+1) + " : PTask | priority : " + to_string(N-i) + " , period : " + to_string(periods[i])
+	  //	  + " , status : DORMANT , cnt : [ 0 / [ " + to_string(computations[i]*1000) + " , " + to_string(computations[i]*1000) + " ] ] >";
+	  + " , status : DORMANT , cnt : [ 0 / [ " + to_string(computations[i]) + " , " + to_string(computations[i]) + " ] ] >";
+      }
+      out << "  eq tl = ";
+      for (i = 0; i < N; i++) {
+	out << tasks[i] << endl;
+	out << "          :: ";
+      }
+      out << "null ." << endl;
+      out << endl;
+
+      out << "  op tinit : -> GlobalSystem ." << endl;
+      out << "  eq tinit = { tl [ 0 / " + to_string(macro) + " ] tsts tHW tisrc } ." << endl;
+      out << endl;
+
+      out << "endtom)" << endl;
+      out << endl;
+
+      out << "(set tick max .)" << endl;
+      out << "(mc tinit |=u []schedulable .)" << endl;
+      out << "quit" << endl;
+      
+      out.close();
+    }
+
+    }
+
+  }
     return 0;
 }
--- Maude/maude24/test-run.cpp
+++ Maude/maude24/test-run.cpp
@@ -4,7 +4,25 @@
 using namespace std;
 
 int main() {
-  system("./maude.linux64 real-time-maude.maude RMS.maude case.maude > test1.log");
-  cout << "good" << endl;
+  int TESTNUM = 1;
+  int TIME = 1;
+  string command;
+
+  for (int N = 5; N <= 20; N++) {
+    cout << "task number " << N << " start:" << endl;
+  for (int i = 0; i < TESTNUM; i++) {
+    for (int j = 0; j < TIME; j++) {
+      command = "./maude.linux64 real-time-maude.maude RMS.maude pow/"
+	+ to_string(N) + "-" + to_string(i) + ".maude > pow-result/"
+	+ to_string(N) + "-" + to_string(i) + "-" + to_string(j) + ".result";
+      system(command.c_str());
+      cout << "Task number " << N << " test " << i << " round " << j << " done" << endl;
+    }
+    cout << "Task number " << N << " test " << i << " is done!" << endl;
+  }
+
+  cout << "Task number " << N << " done!" << endl << endl;
+  }
+
   return 0;
 }
